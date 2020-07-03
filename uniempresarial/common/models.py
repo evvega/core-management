f5a6abbd5975d78601fb309ceb6358af22430648
evelin <evelyn.vega@technoapes.co>
2020-07-03 09:32:16 -0500

    FEAT: :construction: Add new models configs and modules .

--- uniempresarial/common/models.py
+++ uniempresarial/common/models.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+    created_by = models.ForeignKey(User, related_name='+', null=True)
+    modified_by = models.ForeignKey(User, related_name='+', null=True)
+
+    class Meta:
+        abstract = True
+
+
+class StateModel(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+
+    class Meta:
+        abstract = True
+
+
+class ViewModel(object):
+    def save(self, *args, **kwargs):
+        return
+
+    def delete(self, *args, **kwargs):
+        return
--- uniempresarial/config/models.py
+++ uniempresarial/config/models.py
@@ -1,3 +1,100 @@
 from django.db import models
+from common.models import BaseModel
+from common.models import StateModel
+
 
 # Create your models here.
+class Faculty(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
+
+
+class Program(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
+
+
+class ScheduleModel(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
+
+
+class PromotionModel(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
+
+
+class AccountModel(BaseModel):
+    code_id = models.CharField(max_length=15, unique=True)
+    auth_user = models.ForeignKey(User)
+    faculty = models.ForeignKey(Faculty)
+    program = models.ForeignKey(Program)
+    schedule = models.ForeignKey(ScheduleModel)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.code_id, self.auth_user)
+
+
+class ModuleConfig(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    status = models.ForeignKey(StateModel, related_name='status', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
+
+
+class RolConfig(BaseModel):
+    external_id = models.CharField(max_length=15, unique=True, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    status = models.ForeignKey(StateModel, related_name='status', on_delete=models.CASCADE)
+    modules = models.ManyToMany(ModuleConfig, related_name='modules', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
+
+
+class RolUser(BaseModel):
+    account = models.ForeignKey(AccountModel)
+    config = models.ForeignKey(RolConfig)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return '{}{}'.format(self.id, self.name)
