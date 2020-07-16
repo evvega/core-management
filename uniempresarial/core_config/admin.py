7f37341630f533180e6457c70f0f5146d2571ca4
evelin <evelyn.vega@technoapes.co>
2020-07-15 20:36:20 -0500

    FEAT: :bug: Add admin to app core_config.

--- uniempresarial/core_config/admin.py
+++ uniempresarial/core_config/admin.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
+# auto-register all models
+app = apps.get_app_config('core_config')
+
+
+for model_name, model in app.models.items():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
